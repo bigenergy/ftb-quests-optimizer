2645a7d41d7ebd262468758d88774be44fa68419
TexTrue <3140846162@qq.com>
2023-09-15 07:50:46 +0800

    port to architectury

---
+++ common/src/main/java/com/bigenergy/ftbquestsoptimizer/FTBQuestsOptimizer.java
@@ -0,0 +1,15 @@
+package com.bigenergy.ftbquestsoptimizer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FTBQuestsOptimizer {
+
+    public static final String MODID = "ftbqoptimizer";
+    public static final Logger LOGGER = LogManager.getLogger("FTB Quests Optimizer");
+
+    public static void init() {
+        LOGGER.info("Enabling FTB Quests Optimizer");
+    }
+
+}
