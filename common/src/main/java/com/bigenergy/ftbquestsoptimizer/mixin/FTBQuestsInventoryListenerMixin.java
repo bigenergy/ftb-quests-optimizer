aa127110103b7b6cfc6fa1e641341d5b7dbdddb4
Big_Energy <andre90094@gmail.com>
2024-03-17 14:14:46 +0300

    temp fix https://github.com/bigenergy/ftb-quests-optimizer/issues/8

--- common/src/main/java/com/bigenergy/ftbquestsoptimizer/mixin/FTBQuestsInventoryListenerMixin.java
+++ common/src/main/java/com/bigenergy/ftbquestsoptimizer/mixin/FTBQuestsInventoryListenerMixin.java
@@ -1,14 +1,23 @@
 package com.bigenergy.ftbquestsoptimizer.mixin;
 
+import dev.architectury.hooks.level.entity.PlayerHooks;
+import dev.ftb.mods.ftbquests.quest.ServerQuestFile;
+import dev.ftb.mods.ftbquests.quest.TeamData;
+import dev.ftb.mods.ftbquests.quest.task.Task;
 import dev.ftb.mods.ftbquests.util.FTBQuestsInventoryListener;
+import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(value = FTBQuestsInventoryListener.class, remap = false)
 public class FTBQuestsInventoryListenerMixin {
@@ -38,14 +47,39 @@ public class FTBQuestsInventoryListenerMixin {
         return false;
     }
 
-    @Inject(
-        method = "slotChanged",
-        at={@At(value="HEAD")}
-    )
-    public void slotChanged(AbstractContainerMenu menu, int index, ItemStack stack, CallbackInfo ci) {
-        if (!this.tryTick()) {
-            return;
-        }
-    }
+    // todo make new performance test (after https://github.com/FTBTeam/FTB-Quests/commit/296e8ac5eb35634e48c14e35eada9ff550cd837f)
+
+//    /**
+//     * @author Big_Energy
+//     * @reason performance fix
+//     */
+//    @Overwrite
+//    public static void detect(ServerPlayer player, ItemStack craftedItem, long sourceTask) {
+//        ServerQuestFile file = ServerQuestFile.INSTANCE;
+//
+//        if (file == null || PlayerHooks.isFake(player)) {
+//            return;
+//        }
+//
+//        TeamData data = file.getNullableTeamData(FTBTeamsAPI.getPlayerTeamID(player.getUUID()));
+//
+//        if (data == null || data.isLocked()) {
+//            return;
+//        }
+//
+//        file.withPlayerContext(player, () -> file.getSubmitTasks().stream() // WARN: NOT USE ASYNC! -> SERVER CRASH EVERY 5 MIN
+//                .filter(task -> task.id != sourceTask && data.canStartTasks(task.quest))
+//                .forEach(task -> task.submitTask(data, player, craftedItem)));
+//    }
+//
+//
+//    public void slotChanged(AbstractContainerMenu container, int index, ItemStack stack) {
+//        if (!this.tryTick()) {
+//            return;
+//        }
+//        if (!stack.isEmpty() && container.getSlot(index).container == player.getInventory()) {
+//            detect(player, ItemStack.EMPTY, 0);
+//        }
+//    }
 
 }
